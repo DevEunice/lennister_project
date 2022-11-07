e792274b682525eb4fce4577aa8f9e7a9c552192
DevEunice <obebeeunice@gmail.com>
2022-11-07 13:54:17 +0100

    Console.logs removed

--- routes/fees.js
+++ routes/fees.js
@@ -1,10 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const feeController = require("../controller/feeController")
+const feeController = require("../controller/feeController");
 
-/* GET users listing. */
-// router.get('/fees', feeController.getFsc);
-router.post('/fees', feeController.feeConfiguration);
-router.post('/compute-transaction-fee', feeController.feeComputation);
+router.post("/fees", feeController.feeConfiguration);
+router.post("/compute-transaction-fee", feeController.feeComputation);
 
 module.exports = router;
